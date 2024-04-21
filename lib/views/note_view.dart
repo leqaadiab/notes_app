cce05225d5a7649fa8f8bcd172a18846363d4a0f
Leqaa <leqaa.diab@microtec.com.eg>
2024-04-21 15:31:19 +0200

    notes view body

--- lib/views/note_view.dart
+++ lib/views/note_view.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:new_note_application/views/widgets/notes_view_body.dart';
 
 class NoteView extends StatelessWidget {
   const NoteView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: NotesViewBody(),
+    );
   }
+}
