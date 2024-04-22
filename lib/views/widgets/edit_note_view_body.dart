eb24d33278b819372c0b8af218a5cdfa0f9c07bf
Leqaa <leqaa.diab@microtec.com.eg>
2024-04-22 13:07:08 +0200

    note model

--- lib/views/widgets/edit_note_view_body.dart
+++ lib/views/widgets/edit_note_view_body.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:new_note_application/views/widgets/custom_app_bar.dart';
+
+import 'custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          SizedBox(height: 50),
+          CustomAppBar(title: 'Edit Note', icon: Icons.done),
+          SizedBox(height: 50),
+          CustomTextField(hint: 'Title'),
+          SizedBox(height: 30),
+          CustomTextField(
+            hint: 'Content',
+            maxLines: 5,
+          ),
+        ],
+      ),
+    );
   }
 }
