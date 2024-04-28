3d4038d35991b7356a0b5c1a29e3277340c2726d
Leqaa <leqaa.diab@microtec.com.eg>
2024-04-28 12:20:33 +0300

    create cubit

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,11 +1,23 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
 import 'package:new_note_application/models/note_model.dart';
+
+import '../../constants.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
 
-  addNote(NoteModel noteModel) {}
+  addNote(NoteModel noteModel) {
+    emit(AddNoteLoading());
+    try {
+      var noteBox = Hive.box<NoteModel>(kNotesBox);
+      emit(AddNoteSuccess());
+      noteBox.add(noteModel);
+    } catch (e) {
+      emit(AddNoteFailure(e.toString()));
+    }
+  }
 }
